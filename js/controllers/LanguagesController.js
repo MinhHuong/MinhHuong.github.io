8b2507b77cd429b2ef7bed8cd593e56c198d31c2
MinhHuong <minh_huong_148@yahoo.com>
2017-03-05 11:09:56 +0700

    Add directives for languages

--- js/controllers/LanguagesController.js
+++ js/controllers/LanguagesController.js
@@ -1,3 +1,27 @@
-app.controller("LanguagesController", ["$scope", function($scope) {
-	
+app.controller('LanguagesController', ["$scope", function($scope) {
+	$scope.vn = { 
+		img: "img/vn_flag.png", 
+		nameLang: "Vietnamese", 
+		description: "My native language" 
+	};
+	$scope.en = {
+		img: "img/en_flag.png",
+		nameLang: "English",
+		description: "Working proficiency"
+	};
+	$scope.fr = {
+		img: "img/fr_flag.png",
+		nameLang: "French",
+		description: "Bilingual proficinecy"
+	};
 }]);
+
+app.directive('language', function() {
+	return {
+		restrict: 'E',
+		scope: {
+			langInfo: '=info'
+		},
+		templateUrl: 'views/templates/lang-info.html'
+	}
+});
